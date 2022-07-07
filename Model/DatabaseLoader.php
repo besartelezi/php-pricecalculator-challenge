16fe297e02405b43dee7c64334e3de4fa815d411
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-07 16:22:24 +0200

    made DatabaseLoader a class

--- Model/DatabaseLoader.php
+++ Model/DatabaseLoader.php
@@ -1,20 +1,35 @@
 <?php
+declare(strict_types=1);
 // Looking for .env at the root directory
+class DatabaseLoader
+{
+    private string $dbhost;
+    private string $dbname;
+    private string $dbuser;
+    private string $dbpass;
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__),".env");
-$dotenv->load();
+    public function __construct(){
+        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__), ".env");
+        $dotenv->load();
+        $this->dbhost = $_ENV['DATABASE_HOST'];
+        $this->dbname = $_ENV['DATABASE_NAME'];
+        $this->dbuser = $_ENV['DATABASE_USER'];
+        $this->dbpass = $_ENV['DATABASE_PASSWORD'];
+        $this->getConnection();
+    }
 
-// The format required by PDO
+    public function getConnection():void{
+        // The format required by PDO
 //$dbh = new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
-try {
-    $conn = new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
-    $dbname = $_ENV['DATABASE_NAME'];
-    $dbhost = $_ENV['DATABASE_HOST'];
-    //uncomment echo to check if connection was established
-   // echo "Connected to $dbname at $dbhost successfully.";
+        try
+        {
+            $conn = new PDO('mysql:host=' . $this->dbhost . ';dbname=' . $this->dbname, $this->dbuser, $this->dbpass);
+
+            //uncomment echo to check if connection was established
+//             echo "Connected to $this->dbname at $this->dbhost successfully.";
 
 
-    //Commented out example of sql query//
+            //Commented out example of sql query//
 
 //    $sqlResult = $conn->query("select name,price from product");
 //    print_r($sqlResult);
@@ -22,11 +37,16 @@ try {
 //        print "<p>Name: {$row[0]} {$row[1]}</p>";
 //    }
 
-} catch (PDOException $pe) {
-    die("Could not connect to the database $dbname :" . $pe->getMessage());
-}
+        }
+
+        catch
+        (PDOException $pe) {
+            die("Could not connect to the database $this->dbname :" . $pe->getMessage());
+        }
+    }
+
 
 // Retrieve env variable example
 //$userName = $_ENV['DATABASE_USER'];
 //echo $userName;
-
+}
