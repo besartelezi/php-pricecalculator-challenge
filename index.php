95e12dfd19e3f7d6166bb89f6201fbd78ab7e9a2
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-08 12:08:52 +0200

    updated require structure

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 //include all requirements to connect to database
 require_once('vendor\autoload.php');
 
-
 //include all your model files here
-require 'Model/User.php';
+require 'Model/DatabaseLoader.php';
+require 'Model/Product.php';
 
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
+
+
+
+
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
@@ -19,5 +23,7 @@ $controller = new HomepageController();
 if(isset($_GET['page']) && $_GET['page'] === 'info') {
     $controller = new InfoController();
 }
+
+$controller->render($_GET, $_POST);
 
 
