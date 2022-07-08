c633dd0ada9223f672448f16e7d88a60b395ef60
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-08 14:46:05 +0200

    commented out original code + put comment for price-Code for future reference

--- View/homepage.php
+++ View/homepage.php
@@ -2,6 +2,7 @@
 <!-- this is the view, try to put only simple if's and loops here.
 Anything complex should be calculated in the model -->
 <section>
+<!--    original code from boilerplate-->
 <!--    <h4>Hello --><?php //echo $_ENV['DATABASE_NAME']?><!--,</h4>-->
 <!---->
 <!--    <p><a href="index.php?page=info">To info page</a></p>-->
@@ -12,17 +13,14 @@ Anything complex should be calculated in the model -->
 
     <select name="products" id="cars">
         <?php
-
-
         foreach ($productsArray as $product){
             $name = ucfirst($product->getProductName());
-            $price = number_format($product->getProductPrice() / 100, 2);
             echo "<option value=" .$name . ">" . $name . "</option>";
         }
 
-
         ?>
     </select>
-<!--        . $price . "<br>";-->
+<!-- code to get price -->
+<!--    $price = number_format($product->getProductPrice() / 100, 2);-->
 </section>
 <?php require 'includes/footer.php'?>
