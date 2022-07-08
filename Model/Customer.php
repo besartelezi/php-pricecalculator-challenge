d3691da4ea2f03ce24fdf63f1a89df32cf2f78dc
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-08 16:53:56 +0200

    added id parameter to constructor

--- Model/Customer.php
+++ Model/Customer.php
@@ -3,16 +3,23 @@ declare(strict_types=1);
 
 class Customer
 {
+    private int $id;
     private string $firstName;
     private string $lastName;
     private int $groupID;
     private int $fixedDiscount;
     private int $variableDiscount;
 
-    public function __construct(string $firstName,  string $lastName)
+    public function __construct(int $id, string $firstName,  string $lastName)
     {
+        $this->id = $id;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
+    }
+
+    public function getCustomerID()
+    {
+        return $this->id;
     }
 
     /**
