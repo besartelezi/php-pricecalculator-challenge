ed23e28abafc5896f80770e887092c6b9ffbf2f4
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-11 09:46:23 +0200

    added var_dumps to comment in and out, so we have a reference to check if our data is pulling correctly out of the database

--- View/homepage.php
+++ View/homepage.php
@@ -40,8 +40,9 @@ Anything complex should be calculated in the model -->
     <?php
     if (isset($POST['submit'])){
         echo "Product name: " .$productDetails['name'] ."<br>";
-        echo "Price: €" .$productDetails['price']/100;
-
+        echo "Price: €" .$productDetails['price']/100 ."<br>";
+//        var_dump($customerDetails); //to check if group id is correct and if we can get correct info out of database
+//        var_dump($productDetails);
     }
     ?>
 
