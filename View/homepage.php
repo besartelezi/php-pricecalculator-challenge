3be85efa0aafac0bad1db3a45f5004f10ab07998
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-11 15:36:10 +0200

    added var dump to see if information we accessed is correct

--- View/homepage.php
+++ View/homepage.php
@@ -43,8 +43,9 @@ Anything complex should be calculated in the model -->
         echo "Price: €" .$productDetails['price']/100 ."<br>";
 //        var_dump($customerDetails); //to check if group id is correct and if we can get correct info out of database
 //        var_dump($productDetails);
-        var_dump($customerGroupArray);
+//        var_dump($customerGroupArray);
         var_dump($maxVariableDiscount);
+        var_dump($sumFixedDiscounts);
     }
     ?>
 
