6aed65b88dffe2c503a8d6ff37b6b99579cc0880
Besart Elezi <besart.elezi@hotmail.com>
2022-07-11 15:24:24 +0200

    added max variable discount query
    also added query to get all info from database, we might not need it, but it was utilised to check how we could access that information

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -29,6 +29,25 @@ class HomepageController
             $productDetails = $productFetch->fetch();
             $customerFetch = $this->databaseLoader->getConnection()->query("SELECT * FROM customer where id =". $POST['customers']);
             $customerDetails= $customerFetch->fetch();
+
+            $sqlFixed = $this->databaseLoader->getConnection()->query("Alter table customer_group modify fixed_discount int;");
+            $sqlVariable = $this->databaseLoader->getConnection()->query("Alter table customer_group modify variable_discount int;");
+
+            $sqlCustomerGroup = $this->databaseLoader->getConnection()->query("select id, name, ifnull(parent_id,0), ifnull(fixed_discount,0),  ifnull(variable_discount,0) from customer_group
+            where id = (select parent_id from customer_group where id =(select group_id from customer where  id =" . $customerDetails['id'] . "))
+            or id=(select group_id from customer where  id =" . $customerDetails['id'] . ") 
+            or id = (select parent_id from customer_group where id = (select parent_id from customer_group where id =(select group_id from customer where  id =" . $customerDetails['id'] . ")));");
+            $customerGroupArray = [];
+            while ($row = $sqlCustomerGroup->fetch()) {
+                $customerGroupArray[] = new CustomerGroup($row[0], $row[1], $row[2], $row[3], $row[4]);
+            }
+
+            $sqlGetMaxVariableDiscount = $this->databaseLoader->getConnection()->query("select MAX(variable_discount) from customer_group
+            where id = (select parent_id from customer_group where id =(select group_id from customer where  id =" . $customerDetails['id'] . "))
+            or id=(select group_id from customer where  id =" . $customerDetails['id'] . ") 
+            or id = (select parent_id from customer_group where id = (select parent_id from customer_group where id =(select group_id from customer where  id =" . $customerDetails['id'] . ")));");
+            $fetchMaxVariableDiscount = $sqlGetMaxVariableDiscount->fetch();
+            $maxVariableDiscount = $fetchMaxVariableDiscount[0];
         }
 
         // you should not echo anything inside your controller - only assign vars here
